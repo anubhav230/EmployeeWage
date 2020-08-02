bcbad17f10134253381631ed0bdd8f3b0f1c7622
anubhav230 <anubhavsingh230@gmail.com>
2020-08-03 00:34:10 +0530

    ADDED- Code for calculating wage for given days and hrs

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,13 +2,15 @@ let EMP_RATE_PER_HOUR = 20;
 let IS_PART_TIME = 2;
 let IS_FULL_TIME = 1;
 let NUM_OF_WORKING_DAYS = 20;
+let MAX_HRS_IN_MONTH = 100;
 
 let empHrs = 0;
-let empWage = 0;
-let totalWage = 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-
-let empCheck = Math.floor(Math.random() * 10) % 3;
+let totalEmpWage = 0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+	totalWorkingDays++;
+	let empCheck = Math.floor(Math.random() * 10) % 3;
 	switch(empCheck) {
 		case IS_PART_TIME:
 			empHrs = 4;
@@ -19,9 +21,8 @@ let empCheck = Math.floor(Math.random() * 10) % 3;
 		default:
 			empHrs = 0;
 	}
-
-	empWage = empHrs * EMP_RATE_PER_HOUR;
-	totalWage += empWage;
+	totalEmpHrs += empHrs;
 }
-console.log(totalWage);
+totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+console.log(totalEmpWage);
 
