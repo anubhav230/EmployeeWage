63cce71063ea99393edaed48c186b6cb663a3faf
anubhav230 <anubhavsingh230@gmail.com>
2020-08-03 00:05:57 +0530

    ADDED- Code for calculating wage for month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,21 +1,27 @@
 let EMP_RATE_PER_HOUR = 20;
 let IS_PART_TIME = 2;
 let IS_FULL_TIME = 1;
+let NUM_OF_WORKING_DAYS = 20;
 
-let isPresent = 1;
 let empHrs = 0;
 let empWage = 0;
+let totalWage = 0;
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+
 let empCheck = Math.floor(Math.random() * 10) % 3;
-switch(empCheck) {
-	case IS_PART_TIME:
-		empHrs = 4;
-		break;
-	case IS_FULL_TIME:
-		empHrs = 8;
-		break;
-	default:
-		empHrs = 0;
+	switch(empCheck) {
+		case IS_PART_TIME:
+			empHrs = 4;
+			break;
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		default:
+			empHrs = 0;
+	}
+
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	totalWage += empWage;
 }
-empWage = empHrs * EMP_RATE_PER_HOUR;
-console.log(empWage);
+console.log(totalWage);
 
